b1ec3e53a28f3ea510ee5b3ea24d53fc2b4967aa
stan2022fr <47675467+stan2022fr@users.noreply.github.com>
2023-08-09 04:18:52 +0600

    Save CocktailItem in LocalStorage.

--- app/src/main/java/com/happydroid/cocktailbar/presentation/AddCocktailFragment.kt
+++ app/src/main/java/com/happydroid/cocktailbar/presentation/AddCocktailFragment.kt
@@ -1,15 +1,21 @@
 package com.happydroid.cocktailbar.presentation
 
+import android.graphics.drawable.GradientDrawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.happydroid.cocktailbar.R
 import com.happydroid.cocktailbar.data.CocktailRepository
+import com.happydroid.cocktailbar.data.model.CocktailItem
 
 class AddCocktailFragment : Fragment() {
 
@@ -42,6 +48,63 @@ class AddCocktailFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val cocktailNameEditText: EditText = view.findViewById(R.id.cocktail_name_edit)
+        val supportingCocktailName: TextView = view.findViewById(R.id.supporting_cocktail_name_tv)
+
+        cocktailNameEditText.setOnFocusChangeListener { view, hasFocus ->
+            if (hasFocus) {
+                val hintColor = ContextCompat.getColor(requireContext(), R.color.hint_color)
+                cocktailNameEditText.setBackgroundResource(R.drawable.rounded_corner_background)
+                cocktailNameEditText.setTextColor(hintColor)
+                cocktailNameEditText.setHintTextColor(hintColor)
+                supportingCocktailName.setTextColor(hintColor)
+
+            }
+        }
+
+        // Установка обработчика клика на кнопку "Сохранить"
+        val saveButton: Button = view.findViewById(R.id.buttonSave)
+        saveButton.setOnClickListener {
+            if (cocktailNameEditText.text.isNullOrEmpty()) {
+                cocktailNameEditText.clearFocus()
+                val redColor = ContextCompat.getColor(requireContext(), R.color.red)
+                cocktailNameEditText.setTextColor(redColor)
+                cocktailNameEditText.setHintTextColor(redColor)
+                supportingCocktailName.setTextColor(redColor)
+
+                val shape = GradientDrawable()
+                shape.setStroke(2, redColor)
+                shape.cornerRadius = 15f
+                cocktailNameEditText.background = shape
+
+            } else {
+                saveCocktailItem()
+                requireActivity().supportFragmentManager.popBackStack()
+            }
+        }
+
+        // Установка обработчика клика на кнопку "Закрыть"
+        val cancelButton: Button = view.findViewById(R.id.buttonCancel)
+        cancelButton.setOnClickListener {
+            requireActivity().supportFragmentManager.popBackStack()
+        }
+
+    }
+
+    private fun saveCocktailItem() {
+        val cocktailName: EditText = requireView().findViewById(R.id.cocktail_name_edit)
+        val cocktailDescription: EditText = requireView().findViewById(R.id.description_edit)
+        val cocktailRecipe: EditText = requireView().findViewById(R.id.recipe_edit)
+        val cocktailImage_filename = "cocktail1.png"
+        val cocktailItem = CocktailItem(
+            id = 0,
+            name = cocktailName.text.toString(),
+            description = cocktailDescription.text.toString(),
+            recipe = cocktailRecipe.text.toString(),
+            ingredients = listOf(""),
+            imageFileName = cocktailImage_filename
+        )
+        cocktailViewModel.insertCocktail(cocktailItem)
     }
 
 }
--- app/src/main/java/com/happydroid/cocktailbar/presentation/CocktailViewModel.kt
+++ app/src/main/java/com/happydroid/cocktailbar/presentation/CocktailViewModel.kt
@@ -2,15 +2,16 @@ package com.happydroid.cocktailbar.presentation
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import com.happydroid.cocktailbar.data.CocktailRepository
 import com.happydroid.cocktailbar.data.model.CocktailItem
+import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 
 class CocktailViewModel(private val cocktailRepository: CocktailRepository) : ViewModel() {
     val allCocktails: LiveData<List<CocktailItem>> = cocktailRepository.allCocktails
 
-    fun insertCocktail(cocktail: CocktailItem) = viewModelScope.launch {
+    fun insertCocktail(cocktail: CocktailItem) = GlobalScope.launch {
         cocktailRepository.insertCocktail(cocktail)
     }
+
 }
